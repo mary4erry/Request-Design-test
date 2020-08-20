c7a99750be9ef3e3b55dafd21f1f9d83b834a4be
Maria Vatolina <mary4erry@yandex.ru>
2020-08-20 15:50:51 +0300

    end of mobile version

--- packages/swiper.js
+++ packages/swiper.js
@@ -1,13 +1,16 @@
 var swiper = new Swiper('.swiper-container', {
-   slidesPerView: 1,
-   spaceBetween: 30,
-   loop: true,
-   pagination: {
-     el: '.swiper-pagination',
-     clickable: true,
-   },
-   navigation: {
-     nextEl: '.swiper-button-next',
-     prevEl: '.swiper-button-prev',
-   },
- });
+  cssMode: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+slideShow('.slider', {
+  isAutoplay: true
+});
